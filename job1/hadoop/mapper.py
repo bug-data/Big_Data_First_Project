53c6149dc592ab1aec55b55a49595afb02fb5f08
Jerin George Mathew <jerin.edassery95@gmail.com>
2019-04-29 23:48:45 +0200

    filter in range (1998,2018) in job1 mapper

--- job1/hadoop/mapper.py
+++ job1/hadoop/mapper.py
@@ -2,13 +2,19 @@
 
 import sys
 
+startRange = 1998
+endRange = 2018
+
+rangeValues = range(startRange, endRange+1)
+
 for line in sys.stdin:
     #turn each row into a list of strings
     data = line.strip().split(',') 
     if len(data) == 8:
         ticker, _, close, _, low, high, volume, date = data
         
-        #first row of the file
-        if ticker != 'ticker':
+        #check year interval range and if it is the first row of the file
+        year = int(date[0:4])
+        if ticker != 'ticker' and year in rangeValues:
             key = ticker + '_' + date
             print('{}\t{} {} {} {}'.format(key, close, low, high, volume))
