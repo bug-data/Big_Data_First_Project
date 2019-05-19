6f00a053d79ed440f7a27e281cd70f2e4818ca16
Jerin George Mathew <jerin.edassery95@gmail.com>
2019-05-20 00:14:17 +0200

    make job1 faster

--- job1/hadoop/startJob.sh
+++ job1/hadoop/startJob.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-mapred streaming -files mapper.py,reducer.py -mapper mapper.py -reducer reducer.py -input input/historical_stock_prices.csv -output output/job1_hadoop
+mapred streaming \
+		-D stream.num.map.output.key.fields=2 \
+		-files mapper.py,reducer.py \
+		-mapper mapper.py \
+		-reducer reducer.py \
+		-input input/historical_stock_prices.csv \
+		-output output/job1_hadoop
