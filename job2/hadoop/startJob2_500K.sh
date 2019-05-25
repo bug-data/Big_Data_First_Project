d36ac8f524d5087b6c6b26321cc497f18df53e9a
Jerin George Mathew <jerin.edassery95@gmail.com>
2019-05-25 16:30:55 +0200

    add partitioner for the second mapreduce job

--- job2/hadoop/startJob2_500K.sh
+++ job2/hadoop/startJob2_500K.sh
@@ -2,7 +2,8 @@
 
 hadoop jar /usr/lib/hadoop-mapreduce/hadoop-streaming.jar \
 				-D stream.num.map.output.key.fields=3 \
-				-files gs://bug-data/job2/hadoop/mapper.py,gs://bug-data/job2/hadoop/reducer.py,gs://bug-data/input/historical_stocks.csv \
+				-D mapreduce.partition.keypartitioner.options=-k1 \
+                -files gs://bug-data/job2/hadoop/mapper.py,gs://bug-data/job2/hadoop/reducer.py,gs://bug-data/input/historical_stocks.csv \
 				-mapper mapper.py \
 				-reducer reducer.py \
 				-input gs://bug-data/input/historical_stock_prices_500K.csv \
