0f4f696fa2a6d9da4883758b8d2b599c62f524b9
Jerin George Mathew <jerin.edassery95@gmail.com>
2019-05-25 20:05:07 +0200

    set 4 reducers for second mapreduce job

--- job2/hadoop/gcp-scripts/startJob2_1M.sh
+++ job2/hadoop/gcp-scripts/startJob2_1M.sh
@@ -3,6 +3,7 @@
 hadoop jar /usr/lib/hadoop-mapreduce/hadoop-streaming.jar \
 				-D stream.num.map.output.key.fields=3 \
 				-D mapreduce.partition.keypartitioner.options=-k1,1 \
+				-D mapreduce.job.reduces=4 \
 				-files gs://bug-data/job2/hadoop/mapper.py,gs://bug-data/job2/hadoop/reducer.py,gs://bug-data/input/historical_stocks.csv \
 				-mapper mapper.py \
 				-reducer reducer.py \
