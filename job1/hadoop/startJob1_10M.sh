b9c7298b65fee0693e99c7352b17b33484e642c5
Jerin George Mathew <jerin.edassery95@gmail.com>
2019-05-25 14:24:57 +0200

    set only 1 reducer in job1 mapreduce driver scripts

--- job1/hadoop/startJob1_10M.sh
+++ job1/hadoop/startJob1_10M.sh
@@ -2,6 +2,7 @@
 
 hadoop jar /usr/lib/hadoop-mapreduce/hadoop-streaming.jar \
 		-D stream.num.map.output.key.fields=2 \
+		-D mapreduce.job.reduces=1 \
 		-files gs://bug-data/job1/hadoop/mapper.py,gs://bug-data/job1/hadoop/reducer.py \
 		-mapper mapper.py \
 		-reducer reducer.py \
