38a1c0154a05be83e2653c6e030f8f24bc9edffc
Jerin George Mathew <jerin.edassery95@gmail.com>
2019-05-31 22:21:41 +0200

    using 2 reducer for the second mapreduce job

--- job2/mapreduce/startJob2_local.sh
+++ job2/mapreduce/startJob2_local.sh
@@ -3,7 +3,7 @@
 mapred streaming \
 				-D stream.num.map.output.key.fields=3 \
 				-D mapreduce.partition.keypartitioner.options=-k1,1 \
-				-D mapreduce.job.reduces=4 \
+				-D mapreduce.job.reduces=2 \
 				-files mapper.py,reducer.py,../../dataset/historical_stocks.csv \
 				-mapper mapper.py \
 				-reducer reducer.py \
